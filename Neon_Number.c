fac811ebc206f2c5936d84283df5b68f5d5fcf09
sureshreddy999 <119839617+sureshreddy999@users.noreply.github.com>
2022-12-18 16:19:45 +0530

    #include<stdio.h> int main() {     int a,r,sum=0;     scanf("%d" ,&a);     int sq=a*a;     while(sq!=0)     {         r=sq%10;         sum=sum+r;         sq=sq/10;     }     if(sum==a)     {         printf("Neon Number" );     }     else     {         printf("Not Neon Number" );     } }

---
+++ Neon_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,r,sum=0;
+    scanf("%d" ,&a);
+    int sq=a*a;
+    while(sq!=0)
+    {
+        r=sq%10;
+        sum=sum+r;
+        sq=sq/10;
+    }
+    if(sum==a) 
+    {
+        printf("Neon Number" );
+    }
+    else
+    {
+        printf("Not Neon Number" ); 
+    }
+}
