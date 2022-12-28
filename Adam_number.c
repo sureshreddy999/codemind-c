27d46af0c57bb27f9fd8fd012b7ec935055eecd3
sureshreddy999 <119839617+sureshreddy999@users.noreply.github.com>
2022-12-28 20:42:37 +0530

    #include<stdio.h> int main() { 	int a,b,n,i,r,r1,s=0,s1=0; 	scanf("%d",&n); 	a=n*n; 	while(n>0) 	{ 		r=n%10; 		s=s*10+r; 		n=n/10; 	} 	b=s*s; 	while(b>0) 	{ 		r1=b%10; 		s1=s1*10+r1; 		b=b/10; 	} 	if(a==s1) 	{ 		printf("True"); 	} else 	{ 		printf("False"); 	} }

--- Adam_number.c
+++ Adam_number.c
@@ -1,30 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,rem,rev=0,rem2,rev2=0;
-    scanf("%d" ,&n);
-    int sq=n*n;
-    while(n!=0)
-    {
-        rem=n%10;
-        rev=rev*10+rem;
-        n=n/10;
-    }
-    int sq2=rev*rev;
-    while(sq2!=0)
-    {
-        rem2=sq2%10;
-        rev2=rev2*10+rem2;
-        sq2=sq2/10; 
-        
-    } 
-    if(rev2==sq) 
-    {
-        printf("True" );
-    }
-    else 
-    {
-        printf("False" );
-    }
-    
+	int a,b,n,i,r,r1,s=0,s1=0;
+	scanf("%d",&n);
+	a=n*n;
+	while(n>0)
+	{
+		r=n%10;
+		s=s*10+r;
+		n=n/10;
+	}
+	b=s*s;
+	while(b>0)
+	{
+		r1=b%10;
+		s1=s1*10+r1;
+		b=b/10;
+	}
+	if(a==s1)
+	{
+		printf("True");
+	} else
+	{
+		printf("False");
+	}
 }
