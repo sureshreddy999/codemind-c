acd135743112010976d84d118c8c54ec8c1d0f1c
sureshreddy999 <119839617+sureshreddy999@users.noreply.github.com>
2023-01-25 11:25:31 +0530

    #include<stdio.h> int main() {     int i,n,n1,n2;     scanf("%d%d%d" ,&n,&n1,&n2);     for(i=n1;i<=n2;i++)     {         printf("%d x %d = %d" ,n,i,n*i);         printf(" " );     }
    }

---
+++ Multiplication_Table_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,n1,n2;
+    scanf("%d%d%d" ,&n,&n1,&n2); 
+    for(i=n1;i<=n2;i++)
+    {
+        printf("%d x %d = %d" ,n,i,n*i);
+        printf("
+" );
+    }
+    
+}
