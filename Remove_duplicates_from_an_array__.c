e601850c7484408da77314fe4a2e6ecf522bcedd
sureshreddy999 <119839617+sureshreddy999@users.noreply.github.com>
2023-05-20 10:39:46 +0530

    #include <stdio.h> int main () {     int arr[20],i,j,k,n;     scanf (" %d", &n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 for(k=j;k<n-1;k++)                 {                     arr[k]=arr[k+1];                 }                 n--;                 j--;             }         }     }     for(i=0;i<n;i++)     {         printf("%d ",arr[i]);     } }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  
+int main ()  
+{  
+    int arr[20],i,j,k,n;  
+    scanf (" %d", &n);  
+    for(i=0;i<n;i++)  
+    {  
+        scanf("%d",&arr[i]);  
+    }
+    for(i=0;i<n;i++)  
+    {  
+        for(j=i+1;j<n;j++)  
+        {
+            if(arr[i]==arr[j])  
+            {
+                for(k=j;k<n-1;k++)  
+                {  
+                    arr[k]=arr[k+1];  
+                } 
+                n--;  
+                j--;      
+            }  
+        }  
+    }
+    for(i=0;i<n;i++)  
+    {  
+        printf("%d ",arr[i]);  
+    }  
+}  
